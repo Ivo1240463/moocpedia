c229860bf0c972c547cb1aad114f7d15b1410c94
Ivo1240463 <1240463@isep.ipp.pt>
2024-11-18 22:05:15 +0000

    Search bar + Account Management

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MOOCPediaApp;
+using Blazorise.Icons.FontAwesome;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+	//builder.Configuration.Bind("Auth0", options.ProviderOptions);
+	options.ProviderOptions.Authority = "https://dev-gh7hfiov68zh1phc.us.auth0.com";
+	options.ProviderOptions.ClientId = "lMXkjd6tnOn4T51wFk7giyf4qQDHA5U7";
+	options.ProviderOptions.ResponseType = "code";
+});
+
+
+builder.Services.AddRadzenComponents();
+builder.Services.AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
