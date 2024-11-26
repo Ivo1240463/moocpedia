f45a2ac14b1a89f442a488fb986bd08afaa4de25
Ivo1240463 <1240463@isep.ipp.pt>
2024-11-26 03:09:55 +0000

    Home Page v2 (include authors + complete course card)

---
+++ Models/AuthorProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace MOOCPediaApp.Models
+{
+	public class AuthorProfile
+	{
+		public string Name { get; set; } = string.Empty;
+		public string PhotoUrl { get; set; } = "default-person";
+		public string Role { get; set; } = string.Empty;
+	}
+}
---
+++ Models/CourseCard.cs
@@ -0,0 +1,11 @@
+﻿namespace MOOCPediaApp.Models
+{
+	public class CourseCard
+	{
+		public string Title { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string? ImageUrl { get; set; }
+		public int Rate { get; set; } = 0;
+		public int Ratings { get; set; } = 0;
+	}
+}
