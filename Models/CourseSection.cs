c1d5de4aa48496cd18fea589baf1d402cb929233
Simão Amaral <simao.amaral@proton.me>
2024-11-29 14:41:44 +0000

    Course page

---
+++ Models/CourseSection.cs
@@ -0,0 +1,9 @@
+﻿namespace MOOCPediaApp.Models
+{
+    public class CourseSection
+    {
+        public required string Title { get; set; }
+        public bool IsExpanded { get; set; } = false;
+        public required List<Lesson> Lessons { get; set; }
+    }
+}
---
+++ Models/Lesson.cs
@@ -0,0 +1,8 @@
+﻿namespace MOOCPediaApp.Models
+{
+    public class Lesson
+    {
+        public required string Name { get; set; }
+        public required string Duration { get; set; }
+    }
+}
