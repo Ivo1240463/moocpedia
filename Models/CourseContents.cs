31ee5cee20aee0700cbf95729dddce96146490a8
Ivo1240463 <1240463@isep.ipp.pt>
2024-11-29 21:45:51 +0000

    Course Details Page - Initial

---
+++ Models/CourseContents.cs
@@ -0,0 +1,9 @@
+﻿namespace MOOCPediaApp.Models
+{
+	public class CourseContents
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public List<CourseContents> SubContents { get; set; } = new();
+	}
+}
---
+++ Models/CourseDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace MOOCPediaApp.Models
+{
+	public class CourseDetails
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
+		public int Rating;
+		public int Students { get; set; }
+		public int VideoTime { get; set; }
+		public int QuizCount { get; set; }
+		public int ArticlesCount { get; set; }
+		public int DownloadableResourcesCount { get; set; }
+		public List<CourseContents> CourseContents { get; set; } = new();
+	}
+}
